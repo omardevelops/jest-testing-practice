fcebddc675e7ed612b83cf723612559ac8ae00df
omardevelops <omarahmadp91@gmail.com>
2022-06-23 15:52:03 +0400

    added: calculator and tests for all operations

--- src/calculator.js
+++ src/calculator.js
@@ -9,12 +9,15 @@ const calculator = (() => {
     return false;
   };
   const getResult = (result) => {
-    if (isFloat(result)) return parseFloat(result.toFixed(7));
-    else return result;
+    if (isFloat(result)) return parseFloat(result.toFixed(7)); // Approximates floating point arithmetic
+    return result;
   };
   const add = (a, b) => getResult(a + b);
   const subtract = (a, b) => getResult(a - b);
-  const divide = (a, b) => getResult(a / b);
+  const divide = (a, b) => {
+    if (!b) throw new Error('Unexpected dividend value (+0, -0, or NaN)');
+    return getResult(a / b);
+  };
   const multiply = (a, b) => getResult(a * b);
 
   return { add, subtract, divide, multiply };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -53,3 +53,62 @@ test('Subtracts two floating point numbers correctly', () => {
 test('Subtracts two floating point numbers correctly', () => {
   expect(calculator.subtract(1.2, 0.2000001)).toBe(0.9999999);
 });
+
+/* Division */
+test('Divides two integers', () => {
+  expect(calculator.divide(1, 2)).toBe(0.5);
+});
+
+test('Numerator larger than denominator', () => {
+  expect(calculator.divide(5, 3)).toBe(1.6666667);
+});
+
+test('Denominator larger than numerator', () => {
+  expect(calculator.divide(3, 5)).toBe(0.6);
+});
+
+test('Division over zero handled', () => {
+  expect(() => calculator.divide(1, 0)).toThrow(/dividend value/);
+});
+
+test('Division with a negative number (1)', () => {
+  expect(calculator.divide(32, -55)).toBe(-0.5818182);
+});
+
+test('Division with a negative number (2)', () => {
+  expect(calculator.divide(-25, 5)).toBe(-5);
+});
+
+test('Division with a negative number (3)', () => {
+  expect(calculator.divide(-25, -5)).toBe(5);
+});
+
+test('Division with float numbers', () => {
+  expect(calculator.divide(12.25, 2.5)).toBe(4.9);
+});
+
+/* Multiplication */
+test('Multip. with two integers', () => {
+  expect(calculator.multiply(5, 6)).toBe(30);
+});
+
+test('Multip. with zero', () => {
+  const a = Math.random(1, 0) * 100;
+  expect(calculator.multiply(a, 0)).toBe(0);
+});
+
+test('Multip. with float numbers', () => {
+  expect(calculator.multiply(1.25, 7.76)).toBe(9.7);
+});
+
+test('Multip. with a negative number (1)', () => {
+  expect(calculator.multiply(-64, 4)).toBe(-256);
+});
+
+test('Multip. with a negative number (2)', () => {
+  expect(calculator.multiply(32, -4)).toBe(-128);
+});
+
+test('Multip. large numbers', () => {
+  expect(calculator.multiply(234234234, 745674657)).toBe(174662532095607738);
+});
