937ea9996202769a9eee8bdfa302585cb9c78a82
omardevelops <omarahmadp91@gmail.com>
2022-06-27 20:56:47 +0400

    add:  comments

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -5,13 +5,17 @@ const getAverage = (numbers) => {
   return getSum(numbers) / numbers.length;
 };
 const isArrayNumeric = (arr) => {
+  // checks if array contains non-Number elements
   return arr.some((value) => typeof value !== 'number') ? false : true;
 };
 const analyzeArray = (numbers) => {
+  // if NOT numeric, throw error
   if (isArrayNumeric(numbers) === false)
     throw new Error('Error: Input Array contains Non-Number Elements');
+  // if empty, throw error
   if (numbers.length === 0) throw new Error('Error: Input Array is Empty');
 
+  // otherwise, returns analysis of array
   return {
     average: getAverage(numbers),
     min: Math.min(...numbers),
